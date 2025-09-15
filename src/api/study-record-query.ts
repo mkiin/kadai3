51cc6424af6456d2b916e49e2c9518223e1d33b3
mkiin <blckcaties@gmail.com>
2025-09-15 19:56:13 +0900

    try catch実装

--- src/api/study-record-query.ts
+++ src/api/study-record-query.ts
@@ -1,4 +1,6 @@
 // supabaseと通信およびTanstack Queryの関数エクスポート
+
+import { PostgrestError } from "@supabase/supabase-js";
 import { supabase } from "@/lib/supabase";
 import type { TablesInsert, TablesUpdate } from "@/types/database.types";
 
@@ -8,13 +10,21 @@ import type { TablesInsert, TablesUpdate } from "@/types/database.types";
  * @returns 取得した最新の学習記録
  */
 export const getLatestStudyRecords = async (limit: number) => {
-  const { data: result, error } = await supabase
-    .from("study_record")
-    .select("*")
-    .order("created_at", { ascending: false })
-    .limit(limit);
-  if (error) throw error;
-  return result;
+  try {
+    const { data: result, error } = await supabase
+      .from("study_record")
+      .select("*")
+      .order("created_at", { ascending: false })
+      .limit(limit);
+    if (error) throw error;
+    return result;
+  } catch (error) {
+    if (error instanceof PostgrestError) {
+      return error.message;
+    } else {
+      return "エラーが発生しました";
+    }
+  }
 };
 
 /**
@@ -23,14 +33,21 @@ export const getLatestStudyRecords = async (limit: number) => {
  * @returns : result : 作成できた学習記録のデータ
  */
 export const createStudyRecord = async (data: TablesInsert<"study_record">) => {
-  const { data: result, error } = await supabase
-    .from("study_record")
-    .insert(data)
-    .select()
-    .single();
-
-  if (error) throw error;
-  return result;
+  try {
+    const { data: result, error } = await supabase
+      .from("study_record")
+      .insert(data)
+      .select()
+      .single();
+    if (error) throw error;
+    return result;
+  } catch (error) {
+    if (error instanceof PostgrestError) {
+      return error.message;
+    } else {
+      return "エラーが発生しました";
+    }
+  }
 };
 
 /**
@@ -43,24 +60,40 @@ export const updateStudyRecord = async (
   id: string,
   data: TablesUpdate<"study_record">,
 ) => {
-  const { data: result, error } = await supabase
-    .from("study_record")
-    .update(data)
-    .eq("id", id)
-    .select()
-    .single();
+  try {
+    const { data: result, error } = await supabase
+      .from("study_record")
+      .update(data)
+      .eq("id", id)
+      .select()
+      .single();
 
-  if (error) throw error;
-  return result;
+    if (error) throw error;
+    return result;
+  } catch (error) {
+    if (error instanceof PostgrestError) {
+      return error.message;
+    } else {
+      return "エラーが発生しました";
+    }
+  }
 };
 
 export const deleteStudyRecord = async (id: string) => {
-  const { data: result, error } = await supabase
-    .from("study_record")
-    .delete()
-    .eq("id", id)
-    .select()
-    .single();
-  if (error) throw error;
-  return result;
+  try {
+    const { data: result, error } = await supabase
+      .from("study_record")
+      .delete()
+      .eq("id", id)
+      .select()
+      .single();
+    if (error) throw error;
+    return result;
+  } catch (error) {
+    if (error instanceof PostgrestError) {
+      return error.message;
+    } else {
+      return "エラーが発生しました";
+    }
+  }
 };
